bc995fd2e1679b86af14edc03b8b7b8c2261942a
megaurso <bernadogui@hotmail.com>
2023-02-27 13:28:54 -0300

    fix: per page and page

--- src/services/listMovies.services.ts
+++ src/services/listMovies.services.ts
@@ -1,18 +1,30 @@
 import { Repository } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { Movie } from "../entities";
-import { AppError } from "../errors";
 import { TMoviesReturn } from "../interfaces/movies.interfaces";
 import { returnArrayMovieSchema } from "../schemas/movies.schemas";
 
 const listMovies = async (page: any, perPage: any): Promise<TMoviesReturn> => {
   const movieRepo: Repository<Movie> = AppDataSource.getRepository(Movie);
-  const take: number = +perPage || 5;
-  const skip: number = +page || 1;
+  let take: number = +perPage;
+  let skip: number = +page;
 
-  if (Number(skip) < 0 || Number(take) < 0) {
-    throw new AppError("Offset or Limit cannot be negative", 400);
+  if(!Number.isInteger(take)){
+    Math.round(take)
   }
+  if(!Number.isInteger(skip)){
+    Math.round(skip)
+  }
+  if (Math.sign(Number(take)) === -1 ||  Number(take) > 5) {
+    take = 5
+  }
+
+  
+  if(Number(skip) < 0){
+    skip = 1
+  }
+
+
 
   const findMovies: Array<Movie> = await movieRepo.find({
     take,
