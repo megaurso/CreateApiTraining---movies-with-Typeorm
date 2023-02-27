304610c8ccafa790e65ed0fc3edbbf9df459d68e
megaurso <bernadogui@hotmail.com>
2023-02-27 15:19:29 -0300

    fix some bugs

--- src/services/listMovies.services.ts
+++ src/services/listMovies.services.ts
@@ -4,7 +4,7 @@ import { Movie } from "../entities";
 import { TMovieQuerys, TMovieResult } from "../interfaces/movies.interfaces";
 import { returnArrayMovieSchema } from "../schemas/movies.schemas";
 
-const listMovies = async (payload:TMovieQuerys): Promise<TMovieResult> => {
+const listMovies = async (payload: TMovieQuerys): Promise<TMovieResult> => {
   const movieRepo: Repository<Movie> = AppDataSource.getRepository(Movie);
   let perPage = payload.perPage === undefined ? 5 : payload.perPage;
   let page =
@@ -22,19 +22,34 @@ const listMovies = async (payload:TMovieQuerys): Promise<TMovieResult> => {
     Math.ceil(skip);
   }
 
-  if (Math.sign(Number(take)) === -1 || Number(take) > 5) {
+  if (
+    Math.sign(Number(take)) === -1 ||
+    Number(take) > 5 ||
+    Number(take) === 0
+  ) {
     take = 5;
   }
 
   if (Number(skip) <= 0) {
     skip = 1;
   }
-  const totalPages = Math.ceil(await movieRepo.count() / perPage);
+  const totalPages = Math.ceil((await movieRepo.count()) / perPage);
 
   const nextPageFunction = () => {
-    const nextPage = `http://localhost:3000/movies?page=${
+    if (page === 0) {
+      page = 1;
+    }
+
+    
+    console.log(perPage);
+    let nextPage = `http://localhost:3000/movies?page=${
       page + 1
     }&perPage=${+perPage}`;
+    if (Number(perPage) === 0) {
+      nextPage = `http://localhost:3000/movies?page=${
+        page + 1
+      }&perPage=5`;
+    }
     if (totalPages <= page) {
       const nextPage = null;
       return nextPage;
@@ -49,25 +64,27 @@ const listMovies = async (payload:TMovieQuerys): Promise<TMovieResult> => {
 
     if (page <= 1) {
       previusPage = null;
-    }else if(totalPages === page -1 || totalPages >= page){
-      previusPage = `http://localhost:3000/movies?page=${page - 1}&perPage=${+perPage}`;
-    }else{
+    } else if (totalPages === page - 1 || totalPages >= page) {
+      previusPage = `http://localhost:3000/movies?page=${
+        page - 1
+      }&perPage=${+perPage}`;
+    } else {
       previusPage = null;
     }
     return previusPage;
   };
 
-  const sort = payload.sort === undefined ? "id" : payload.sort
-  let order = payload.order === undefined ? "ASC" : payload.order
-  if(!payload.sort){
-    order = "ASC"
+  const sort = payload.sort === undefined ? "id" : payload.sort;
+  let order = payload.order === undefined ? "ASC" : payload.order;
+  if (!payload.sort) {
+    order = "ASC";
   }
 
   const findMovies: Array<Movie> = await movieRepo.find({
     take,
     skip: take * (skip - 1),
     order: {
-      [sort]:order
+      [sort]: order,
     },
   });
 
